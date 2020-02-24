365e245981c07e36f4ac886ca8ba0f57cf940010
James <aslan.jnn@gmail.com>
2020-02-24 21:51:42 +1100

    Integrated SFTP object files to dropbear server

---
+++ startsftp.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pwd.h>
+
+#include "sftp.h"
+#include "xmalloc.h"
+
+int
+sftp_main(int argc, char **argv)
+{
+	struct passwd *user_pw;
+
+	ssh_malloc_init();	/* must be called before any mallocs */
+	/* Ensure that fds 0, 1 and 2 are open or directed to /dev/null */
+	//sanitise_stdfd();
+
+	if ((user_pw = getpwuid(getuid())) == NULL) {
+		fprintf(stderr, "No user found for uid %lu\n",
+		    (u_long)getuid());
+		return 1;
+	}
+
+	return (sftp_server_main(argc, argv, user_pw));
+}
---
+++ startsftp.h
@@ -0,0 +1,6 @@
+#ifndef DROPBEAR_STARTSFTP_H
+#define DROPBEAR_STARTSFTP_H
+
+int sftp_main(int argc, char** argv);
+
+#endif
